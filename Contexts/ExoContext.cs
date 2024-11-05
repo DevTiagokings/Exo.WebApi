629a309e75cb945036b253f5e0290969f22ab001
Tiago Gomes do Nascimento <Gomes.gt001@gmail.com>
2024-11-05 18:50:29 -0300

    Estrutura de Api com context

---
+++ Contexts/ExoContext.cs
@@ -0,0 +1,36 @@
+using Exo.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+
+namespace Exo.WebApi.Contexts
+{
+    public class ExoContext : DbContext
+    {
+        public ExoContext()
+        {
+        }
+        public ExoContext(DbContextOptions<ExoContext> options) : base(options)
+            {
+            }
+            protected override void OnConfiguring(DbContextOptionsBuilder OptionsBuilder)
+            {
+                if(!OptionsBuilder.IsConfigured)
+                {
+                    // Essa string de conexão depende da sua máquina.
+                    OptionsBuilder.UseSqlServer("server=localhost\\SQLEXPRESS;" +
+
+                    "Database=ExoApi;Trusted_connection=True;");
+                
+                  // Exemplo 1 de string de conexão:
+                  //User ID=sa;Password=Gomes0906;Server=localhost;Database=ExoApi;
+                  //trusted_Connection=False;
+
+                 // Exemplo 2 de string de conexão:
+                 // Server=localhost\\SQLEXPRESS;Database=ExoApi;Trusted_Connection=True;
+                }
+            }    
+            public DbSet<Projeto> Projetos {get; set; }
+    }
+ 
+}
