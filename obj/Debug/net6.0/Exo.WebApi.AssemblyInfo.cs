e64ad528518be0fc8b76e05fe64ffb83f3b1aa81
Tiago Gomes do Nascimento <Gomes.gt001@gmail.com>
2024-11-17 10:42:53 -0300

    Atualização do projeto

--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+629a309e75cb945036b253f5e0290969f22ab001")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+916dba2204208362e824b58aeab25025f3ffede0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
